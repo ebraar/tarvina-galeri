409411e97d1b322cb47fa709cdddf94d81f3ed10
ebraar <ebbet468@gmail.com>
2024-05-11 11:28:08 +0300

    sayfa düzeni için update

--- tarvina-galeri/app/page.js
+++ tarvina-galeri/app/page.js
@@ -1,13 +1,22 @@
 import Navbar from "/components/navbar";
 import CategoryMenu from "/components/kategori";
+import Photos from "/components/foto";
 
 export default function Home() {
   return (
-    <>
+<>
     <Navbar/>
-    <div className="flex">
-      <CategoryMenu/>
+    <div className="pt-16">  
+      <div className="flex flex-row items-start gap-8">
+        <div className="w-1/4"> 
+          <CategoryMenu /> 
+        </div>
+        <div className="w-3/4"> 
+          <Photos /> 
+        </div>
+      </div>
     </div>
-   </>
+</>
+
   );
 }
